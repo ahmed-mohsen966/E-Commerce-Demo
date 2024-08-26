ea569213d7166c1906f24353995fb183cad252cb
ahmed mohsen <ah.mohsen75@gmail.com>
2024-08-26 11:39:14 +0300

    Add Model and viewmodel

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StoreData } from '../../ViewModels/store-data';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  storeInfo : StoreData;
+
+  constructor(){
+    this.storeInfo = new StoreData("amazon store" , "https://picsum.photos/200/300",["Alex", "Giza","Maadi" , "Gouna"]);
+  }
 
 }
