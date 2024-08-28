735ec7314ff15834fbe2014f90f7bd0a57cbb869
ahmed mohsen <ah.mohsen75@gmail.com>
2024-08-28 14:51:22 +0300

    add filter products by category Id

--- src/app/Components/products/products.component.ts
+++ src/app/Components/products/products.component.ts
@@ -12,11 +12,19 @@ orderTotalPrice:number = 0;
 prdList : IProducts[];
 catList: ICategory[];
 selectdCatId:number = 0;
+filteredProducts: IProducts[] = [];
 constructor(){
   this.prdList =[
-    {id:100,name:'Dell' , price: 5000, quantity:1,imgURL: "https://fakeimg.pl/200x100" , categoryId:1},
-    {id:100,name:'Lenovo' , price: 25000, quantity:2,imgURL: "https://fakeimg.pl/200x100" , categoryId:2},
-    {id:100,name:'Mac' , price: 10000, quantity:5,imgURL: "https://fakeimg.pl/200x100" , categoryId:3}
+    {id:100,name:'Dell' , price: 5000, quantity:1,imgURL: "https://fakeimg.pl/200x100" , categoryId:1000},
+    {id:100,name:'Lenovo' , price: 25000, quantity:2,imgURL: "https://fakeimg.pl/200x100" , categoryId:2000},
+    {id:100,name:'Mac' , price: 10000, quantity:5,imgURL: "https://fakeimg.pl/200x100" , categoryId:3000},
+    {id:100,name:'hp' , price: 25000, quantity:2,imgURL: "https://fakeimg.pl/200x100" , categoryId:3000},
+    {id:100,name:'toshiba' , price: 25000, quantity:2,imgURL: "https://fakeimg.pl/200x100" , categoryId:1000},
+    {id:100,name:'Lenovo 2' , price: 25000, quantity:2,imgURL: "https://fakeimg.pl/200x100" , categoryId:2000},
+    {id:100,name:'Lenovo 3' , price: 25000, quantity:2,imgURL: "https://fakeimg.pl/200x100" , categoryId:2000},
+    {id:100,name:'Lenovo 4' , price: 25000, quantity:2,imgURL: "https://fakeimg.pl/200x100" , categoryId:1000},
+    {id:100,name:'Lenovo 5' , price: 25000, quantity:2,imgURL: "https://fakeimg.pl/200x100" , categoryId:4000},
+
   ];
 
   this.catList = [
@@ -33,7 +41,11 @@ BuyItem(ProductPrice:number , PrdCount:any){
   this.orderTotalPrice = itemsCount * ProductPrice;
 
 }
-ChangeCat(){
-  this.selectdCatId = 1000;
+// ChangeCat(){
+//   this.selectdCatId = 1000;
+// }
+ChangeCatFilter(){
+  debugger
+  this.filteredProducts = this.prdList.filter(prd => prd.categoryId == this.selectdCatId);
 }
 }
