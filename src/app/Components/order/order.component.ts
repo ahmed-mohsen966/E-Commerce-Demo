9b09d36064d4fc8bdccd3af2a20cb52be0969140
ahmed mohsen <ah.mohsen75@gmail.com>
2024-09-01 22:40:42 +0300

    using of viewchild

--- src/app/Components/order/order.component.ts
+++ src/app/Components/order/order.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { ICategory } from '../../Models/icategory';
 import { StoreData } from '../../ViewModels/store-data';
+import { throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
-export class OrderComponent {
+export class OrderComponent implements AfterViewInit {
   catList: ICategory[];
   selectdCatId: number = 0;
   orderTotalPrice: number = 0;
   buyItems: StoreData[];
+
+  @ViewChild('ClientNameInput') clientNameElem!: ElementRef;
   constructor() {
     this.buyItems = [];
     this.catList = [
@@ -20,6 +23,9 @@ export class OrderComponent {
       { id: 3000, name: "PC" },
       { id: 4000, name: "Other" },
     ]
+  }
+  ngAfterViewInit(): void {
+    this.clientNameElem.nativeElement.value = "Enter your name here";
   }
 
   onTotalPriceChanged(totalPrice: number) {
