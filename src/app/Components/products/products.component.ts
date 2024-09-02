06967aa1d3cad269c955447d72095ffc5b6796f9
ahmed mohsen <ah.mohsen75@gmail.com>
2024-09-02 23:11:54 +0300

    Create Static Product Service.

--- src/app/Components/products/products.component.ts
+++ src/app/Components/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, ɵUSE_RUNTIME_DEPS_TRACKER_FOR_JIT } from '@angular/core';
 import { IProducts } from '../../Models/iproducts';
 import { StoreData } from '../../ViewModels/store-data';
+import { StaticProductsService } from '../../Services/static-products.service';
 
 @Component({
   selector: 'app-products',
@@ -9,32 +10,22 @@ import { StoreData } from '../../ViewModels/store-data';
 })
 export class ProductsComponent implements OnChanges, OnInit {
   orderTotalPrice: number = 0;
-  prdList: IProducts[];
   filteredProducts: IProducts[] = [];
+  prdList: IProducts[] = [];
   @Input() CatId: number = 0;
   @Output() onTotalPricechanged: EventEmitter<number>;
   @Output() onBuyItem: EventEmitter<StoreData>;
-  constructor() {
+  constructor(private productService: StaticProductsService ) {
     this.onTotalPricechanged = new EventEmitter<number>();
     this.onBuyItem = new EventEmitter<StoreData>();
-    this.prdList = [
-      { id: 100, name: 'Dell', price: 5000, quantity: 1, imgURL: "https://fakeimg.pl/200x100", categoryId: 1000 },
-      { id: 100, name: 'Lenovo', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 2000 },
-      { id: 100, name: 'Mac', price: 10000, quantity: 5, imgURL: "https://fakeimg.pl/200x100", categoryId: 3000 },
-      { id: 100, name: 'hp', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 3000 },
-      { id: 100, name: 'toshiba', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 1000 },
-      { id: 100, name: 'Lenovo 2', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 2000 },
-      { id: 100, name: 'Lenovo 3', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 2000 },
-      { id: 100, name: 'Lenovo 4', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 1000 },
-      { id: 100, name: 'Lenovo 5', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 4000 },
 
-    ];
   }
   ngOnInit(): void {
     this.filteredProducts = this.prdList;
   }
   ngOnChanges(): void {
-    this.ChangeCatFilter();
+    //this.ChangeCatFilter();
+    this.filteredProducts = this.productService.GetProductByCatId(this.CatId);
   }
 
   BuyItem(id: number, PrdCount: any) {
---
+++ src/app/Services/static-products.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { IProducts } from '../Models/iproducts';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StaticProductsService {
+  private prdList: IProducts[];
+
+  constructor() {
+    this.prdList = [
+      { id: 100, name: 'Dell', price: 5000, quantity: 1, imgURL: "https://fakeimg.pl/200x100", categoryId: 1000 },
+      { id: 100, name: 'Lenovo', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 2000 },
+      { id: 100, name: 'Mac', price: 10000, quantity: 5, imgURL: "https://fakeimg.pl/200x100", categoryId: 3000 },
+      { id: 100, name: 'hp', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 3000 },
+      { id: 100, name: 'toshiba', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 1000 },
+      { id: 100, name: 'Lenovo 2', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 2000 },
+      { id: 100, name: 'Lenovo 3', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 2000 },
+      { id: 100, name: 'Lenovo 4', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 1000 },
+      { id: 100, name: 'Lenovo 5', price: 25000, quantity: 2, imgURL: "https://fakeimg.pl/200x100", categoryId: 4000 },
+
+    ];
+  }
+
+  GetAllProducts(): IProducts[] {
+    return this.prdList;
+  }
+
+  GetProductByCatId(CatId: number): IProducts[] {
+    if (CatId == 0)
+      return this.prdList;
+    else
+      return this.prdList.filter(prd => prd.categoryId == CatId);
+  }
+
+  GetProductById(id: number): IProducts | null {
+    if (id != null) {
+      let prd = this.prdList.find(prd => prd.id == id);
+      return prd ? prd : null;
+    } else {
+      return null;
+    }
+  }
+
+  Create(prd: IProducts): void {
+    this.prdList.push(prd);
+  }
+}
