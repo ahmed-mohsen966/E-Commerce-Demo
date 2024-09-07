23bedf703b8f28abf9c31aa485bcd720beed8db9
ahmed mohsen <ah.mohsen75@gmail.com>
2024-09-07 11:43:07 +0300

    implement missed functions

--- src/app/Components/ProductDetails/ProductDetails.component.ts
+++ src/app/Components/ProductDetails/ProductDetails.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProducts } from '../../Models/iproducts';
+import { StaticProductsService } from '../../Services/static-products.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-ProductDetails',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ProductDetails.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  prdId: number = 0;
+  product: IProducts | null = null;
+  constructor(private activatedRoute: ActivatedRoute , private staticProductService : StaticProductsService
+    , private location: Location
+  ) {
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.prdId = Number(this.activatedRoute.snapshot.paramMap.get('pid'));
+    this.product = this.staticProductService.GetProductById(this.prdId);
+  }
+  Back(){
+    this.location.back();
+  }
 }
